f3c8ea22f7ba0e42f959e15dec4fd369044690f4
taip315 <beaeaeat@gmail.com>
2021-06-14 08:50:34 +0900

    データベース作成

--- db/migrate/20210613052429_create_categories.rb
+++ db/migrate/20210613052429_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
-      t.sring :name, null: false, unique: true
+      t.string :name, null: false, unique: true
       t.timestamps
     end
   end

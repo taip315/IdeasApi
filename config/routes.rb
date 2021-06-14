46ac312933f20a6a87e5814f5fdd4010aaa7fdea
taip315 <beaeaeat@gmail.com>
2021-06-14 10:03:40 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :category, only: :index
-resources :idea, only: :create
+resources :categories, only: :index
+resources :ideas, only: :create
 end
